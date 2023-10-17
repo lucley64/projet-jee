8fdc228090e361f85a0d8691460c5792c117ae67
lucley64 <luc.leydert@gmail.com>
2023-10-17 09:47:58 +0200

    first commit

---
+++ src/test/java/com/example/jee/projetjee/ProjetJeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jee.projetjee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetJeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
