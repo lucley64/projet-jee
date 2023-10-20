02227a71e622f6c1972dd10d6b2be7c7fde5b8f6
lucley64 <luc.leydert@gmail.com>
2023-10-20 10:44:19 +0200

    Changes by shinlil

--- src/main/java/com/example/jee/projetjee/controllers/ScienceController.java
+++ src/main/java/com/example/jee/projetjee/controllers/ScienceController.java
@@ -1,11 +1,17 @@
 package com.example.jee.projetjee.controllers;
 
-import org.springframework.stereotype.Controller;
+import com.example.jee.projetjee.data.Category;
+import com.example.jee.projetjee.data.Science;
+import com.example.jee.projetjee.repositories.ScienceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
 public class ScienceController {
+
+    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+    @Autowired
+    private ScienceRepository scienceRepository;
 
     @RequestMapping("/")
     String index(Model model){
@@ -14,4 +20,5 @@ public class ScienceController {
 
         return "index";
     }
+
 }
