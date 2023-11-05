55b710dc7fb1b0ab3f5ee5fe50e6eb9bcac48625
lucley64 <luc.leydert@gmail.com>
2023-11-05 15:23:23 +0100

    login signup

--- src/main/java/com/example/jee/projetjee/ProjetJeeApplication.java
+++ src/main/java/com/example/jee/projetjee/ProjetJeeApplication.java
@@ -1,11 +1,15 @@
 package com.example.jee.projetjee;
 
 import com.example.jee.projetjee.data.Role;
+import com.example.jee.projetjee.data.User;
 import com.example.jee.projetjee.repositories.RoleRepository;
+import com.example.jee.projetjee.repositories.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import java.util.Set;
 
 @SpringBootApplication
 public class ProjetJeeApplication {
@@ -15,7 +19,8 @@ public class ProjetJeeApplication {
     }
 
     @Bean
-    public CommandLineRunner demo(RoleRepository roleRepository){
+    public CommandLineRunner demo(RoleRepository roleRepository,
+                                  UserRepository userRepository){
         return args -> {
             var admin = roleRepository.findByName("ROLE_ADMINISTRATOR");
             if (admin.isEmpty()){
@@ -28,6 +33,17 @@ public class ProjetJeeApplication {
                 Role role = new Role();
                 role.setName("USER");
                 roleRepository.save(role);
+            }
+            var userAdmin = userRepository.findByUsername("admin");
+            admin = roleRepository.findByName("ROLE_ADMINISTRATOR");
+            if (userAdmin.isEmpty() && admin.isPresent()){
+                User user1 = new User();
+                user1.setUsername("admin");
+                user1.setEmail("admin");
+                user1.setPassword("admin");
+                user1.setRoles(Set.of(admin.get()));
+                userRepository.save(user1);
+
             }
         };
     }
